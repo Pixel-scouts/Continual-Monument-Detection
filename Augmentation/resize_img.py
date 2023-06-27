5342cc67175ea119cbe24908e704a573d1ea3c42
St-Luciferr <santosh.762417@thc.tu.edu.np>
2023-06-28 02:56:30 +0545

    small modification

--- Augmentation/resize_img.py
+++ Augmentation/resize_img.py
@@ -5,16 +5,16 @@ import numpy as np
 import os
 
 # replace with the folder containing images to resize
-folder_path = 'E:\Major Project\Images\Basantapur'  
+folder_path = 'E:\Major Project\Images\Original Images\\Pashupati Baneshwor Naxal'  
 
 # replace with the actual folder name to store resized images
-output_folder = "E:\Major Project\Images\Basantapur_resized"
+output_folder = "E:\Major Project\Images\Resized Images\\Pashupati Baneshwor Naxal"
 os.makedirs(output_folder, exist_ok=True)
 
 
 files = os.listdir(folder_path)
 image_files = [file for file in files if file.lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
-print(image_files)
+# print(image_files)
 for image_file in image_files:
     image_path = os.path.join(folder_path, image_file)
     # Load each imageh
@@ -40,3 +40,4 @@ for image_file in image_files:
     output_path = os.path.join(output_folder, image_file)
     cv2.imwrite(output_path, res_img_np)
 
+print("resizing complete")
